c02f6dd4c3c7b1456df1e3435f18e6cbb7a66e35
Max-GO-IT <terletskymaxim@ukr.net>
2024-04-18 12:18:37 +0300

    1 урок по js
    1 урок по JS

---
+++ js/JS task-3.js
@@ -0,0 +1,12 @@
+// function getElementWidth(content,padding,border) {
+//    let Sum = Number(content.replace("px","")) 
+//    + Number(padding.replace("px",""))* 2
+//    + Number(border .replace("px","")) * 2;
+  
+//    return (Sum)
+// }
+// console.log(getElementWidth("50px", "8px", "4px")); 
+// console.log(getElementWidth("60px", "12px", "8.5px")); 
+// console.log(getElementWidth("200px", "0px", "0px"));
+
+// console.log(productName.slice(0, 4)); 
